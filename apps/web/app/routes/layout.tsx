236541bbde7c1ee83d8666eb304ca64c2e4c7f6a
Tashi Tobgay Dakpa <tashitobgaylibra@gmail.com>
2025-09-26 19:07:42 +0600

    placeholder

--- apps/web/app/routes/layout.tsx
+++ apps/web/app/routes/layout.tsx
@@ -5,12 +5,15 @@ import { BackgroundGradientAnimation } from 'components/ui/background-gradient-a
 
 export default function Layout() {
   return (
-    <BackgroundGradientAnimation>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </BackgroundGradientAnimation>
+    <div className='h-full w-full relative'>
+      <BackgroundGradientAnimation containerClassName='absolute top-0 left-0'></BackgroundGradientAnimation>
+      <div className='h-full flex flex-col'>
+        <Header />
+        <main className='flex-1'>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
   );
 }
--- apps/web/app/welcome/welcome.tsx
+++ apps/web/app/welcome/welcome.tsx
@@ -1,3 +1,9 @@
 export function Welcome() {
-  return <section></section>;
+  return (
+    <section className='flex items-center justify-center h-full'>
+      <div className='isolate aspect-square md:aspect-video w-52 md:w-96 rounded-xl bg-black/20 shadow-lg ring-1 ring-black/5 pointer-events-none flex items-center justify-center backdrop-blur-sm'>
+        <h1 className='text-4xl font-bold text-center text'>Coming Soon...</h1>
+      </div>
+    </section>
+  );
 }
